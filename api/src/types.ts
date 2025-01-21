f3703b195d16cfe4d93a893aa25dac7fe619bed0
Clara <honjes@protonmail.com>
2025-01-21 17:30:04 +0100

    feat: add Type for creation of dbValues

--- api/src/types.ts
+++ api/src/types.ts
@@ -1,4 +1,6 @@
+import { createInsertSchema } from 'drizzle-typebox'
 import { t } from 'elysia'
+import { posts, users } from './db/schema'
 
 export const viablePodProviders = t.Enum({
   'http://localhost:3000': 'http://localhost:3000'
@@ -9,3 +11,7 @@ export interface PodProviderResponse {
   webId: string
   newUser: boolean
 }
+
+// DB types
+export const _createPost = createInsertSchema(posts)
+export const _createUser = createInsertSchema(users)
