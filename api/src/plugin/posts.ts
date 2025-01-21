7db0496e08e6b0d135de51af742b1114114746e4
Clara <honjes@protonmail.com>
2025-01-21 20:07:34 +0100

    add get route for posts

---
+++ api/src/plugin/posts.ts
@@ -0,0 +1,71 @@
+import { eq } from 'drizzle-orm'
+import Elysia, { t } from 'elysia'
+import { posts } from '../db/schema'
+import ActivityPod from '../services/ActivityPod'
+import { _createPost, _selectposts, selectQueryObject } from '../types'
+import { db } from '..'
+import User from '../decorater/User'
+
+const postsRoutes = new Elysia()
+  .decorate('user', new User())
+  .guard({
+    isSignedIn: true
+  })
+  .post(
+    '/posts',
+    async ({ error, body, user }) => {
+      const { content, isPublic } = body
+
+      const addressats = [`${user.userId}/followers`]
+      if (isPublic) addressats.push('https://www.w3.org/ns/activitystreams#Public')
+
+      const post = {
+        '@context': 'https://www.w3.org/ns/activitystreams',
+        type: 'Note',
+        attributedTo: user.userId,
+        content: content,
+        to: addressats
+      }
+
+      try {
+        // create the post in the pod
+        await ActivityPod.createPost(user, post)
+        // insert the post into the database
+        await db.insert(posts).values({
+          content,
+          isPublic
+        })
+      } catch (e) {
+        console.error('Error while creating the post: ', e)
+        return error(500, 'Error while creating the post')
+      }
+
+      return 'Successfully created the post'
+    },
+    {
+      body: t.Omit(_createPost, ['id', 'created_at']),
+      response: {
+        200: t.String(),
+        500: t.String()
+      },
+      detail: 'Creates a new post',
+      isSignedIn: true
+    }
+  )
+  .get(
+    '/posts',
+    async ({ query: { limit, offset } }) => {
+      const postsQuery = await db.select().from(posts).where(eq(posts.isPublic, true)).limit(limit).offset(offset)
+      return postsQuery
+    },
+    {
+      detail: 'Returns all public posts',
+      isSignedIn: true,
+      query: selectQueryObject,
+      response: {
+        200: t.Array(_selectposts)
+      }
+    }
+  )
+
+export default postsRoutes
