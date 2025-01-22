90386c199f8e7a8ce1b21a2039e5e13d7f662855
briidee3 <briidee3@gmail.com>
2025-01-21 22:04:34 -0500

    Commented out hashtag actions and api

--- backend/services/app.service.js
+++ backend/services/app.service.js
@@ -49,7 +49,7 @@ module.exports = {
       }
     },
     queueServiceUrl: CONFIG.QUEUE_SERVICE_URL
-  },
+  }
   /*actions: {
     async fetchPostsByHashtag(ctx) {
       const { tag } = ctx.params;
@@ -71,5 +71,34 @@ module.exports = {
       const results = await ctx.call("triplestore.query", { query });
       return results.map(row => row.activity);
     },
+  },
+  actions: {
+    async fetchPostsByHashtag(ctx) {
+
+      const tagUris = await Promise.all(
+        hashtags.map(async (name) => {
+          //const tagUri = `${triplestore.baseUrl}/tags/${name}`;
+          const tagUri = `${process.env.SEMAPPS_HOME_URL}/tags/${name}`;
+          await triplestore.insertOrUpdate(tagUri, {
+            "@context": "http://www.w3.org/ns/activitystreams#tag",
+            type: "Hashtag",
+            name,
+          });
+          return tagUri;
+        })
+      );
+
+      return tagUris;
+    },
+  },
+  api: {
+    routes: [
+      {
+        path: "/tag/:tag/posts",
+        aliases: {
+          "GET /": "core.fetchPostsByHashtag",
+        },
+      },
+    ],
   },*/
 };
