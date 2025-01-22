351ab2af5c03df7fc59c4a4663c90e45a2f99e28
Clara <honjes@protonmail.com>
2025-01-22 14:10:22 +0100

    add tailwind to frontend

--- vueFrontend/src/main.ts
+++ vueFrontend/src/main.ts
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Styles
+import './assets/style.css'
+
 const app = createApp(App).use(createPinia()).use(router)
 
 app.mount('#app')
