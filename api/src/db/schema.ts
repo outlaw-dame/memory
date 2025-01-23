e851126a633e273be10ba2699549682cc9d55316
Clara <honjes@protonmail.com>
2025-01-23 12:25:02 +0100

    add replations from posts to users

--- api/src/db/schema.ts
+++ api/src/db/schema.ts
@@ -1,14 +1,21 @@
-import { boolean, text, serial, pgTable as table, timestamp } from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
+import { boolean, text, serial, pgTable as table, timestamp, integer } from 'drizzle-orm/pg-core'
 
 export const users = table('users', {
   id: serial().primaryKey(),
   name: text().notNull(),
-  webId: text('web_id').notNull().unique()
+  webId: text('web_id').notNull().unique(),
+  providerEndpoint: text('provider_endpoint').notNull(),
 })
+
+export const usersRelations = relations(users, ({one}) => ({
+  posts: one(posts),
+}))
 
 export const posts = table('posts', {
   id: serial().primaryKey(),
   content: text().notNull(),
   createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
-  isPublic: boolean('is_public').notNull()
+  isPublic: boolean('is_public').notNull(),
+  authorId: integer('author_id').notNull().references(() => users.id),
 })
