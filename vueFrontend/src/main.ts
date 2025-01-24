d202db640398d7795fc787dad023898c7be436c4
Clara <honjes@protonmail.com>
2025-01-24 11:53:11 +0100

    add vuesax

--- vueFrontend/src/main.ts
+++ vueFrontend/src/main.ts
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Vuesax from 'vuesax-alpha'
 
 import App from './App.vue'
 import router from './router'
 
 // Styles
+import 'vuesax-alpha/dist/index.css'
 import './assets/style.css'
 
-const app = createApp(App).use(createPinia()).use(router)
+const app = createApp(App).use(createPinia()).use(router).use(Vuesax, {})
 
 app.mount('#app')
