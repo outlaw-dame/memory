72c5f78ab3bb9cd4a17644274b77dfa64d282f82
Clara <honjes@protonmail.com>
2025-01-25 11:28:31 +0100

    add a controlbar to all pages

--- vueFrontend/src/App.vue
+++ vueFrontend/src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import ControlBar from '@/components/ControlBar.vue'
 </script>
 
 <template>
   <div class="memoryContainer grid h-lvh grid-rows-[fit-content(100%)_auto] items-stretch px-6">
-    <header></header>
+    <header>
+      <ControlBar />
+    </header>
     <main>
       <RouterView />
     </main>
---
+++ vueFrontend/src/components/ControlBar.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/authStore'
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+// External
+const authStore = useAuthStore()
+const route = useRoute()
+const router = useRouter()
+// When the route changes, update the title
+router.afterEach(getHeader)
+
+// Variables
+const title = ref('memory.')
+
+// Functions
+function getHeader() {
+  if (route.name === 'signin' || route.name === 'signup') {
+    title.value = 'memory.'
+  }
+}
+</script>
+
+<template>
+  <div class="flex justify-between pt-4">
+    <div></div>
+    <h1 class="text-pageTitle font-[Butler]">{{ title }}</h1>
+    <div></div>
+  </div>
+</template>
