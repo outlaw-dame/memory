083acd1649ddaefc040f8fb86055ef0797b665e9
Clara <honjes@protonmail.com>
2025-01-27 08:48:27 +0100

    add view for post joined with author

--- api/src/db/schema.ts
+++ api/src/db/schema.ts
@@ -1,5 +1,5 @@
-import { relations } from 'drizzle-orm'
-import { boolean, text, serial, pgTable as table, timestamp, integer } from 'drizzle-orm/pg-core'
+import { relations, eq, sql } from 'drizzle-orm'
+import { boolean, text, serial, pgTable as table, timestamp, integer, pgView } from 'drizzle-orm/pg-core'
 
 export const users = table('users', {
   id: serial().primaryKey(),
@@ -19,3 +19,21 @@ export const posts = table('posts', {
   isPublic: boolean('is_public').notNull(),
   authorId: integer('author_id').notNull().references(() => users.id),
 })
+
+export const postsView = pgView('posts_view', {
+  id: serial().primaryKey(),
+  content: text().notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
+  isPublic: boolean('is_public').notNull(),
+  authorId: integer('author_id').notNull().references(() => users.id),
+  authorName: text("author_name").notNull(),
+  authorWebId: text('author_web_id').notNull().unique(),
+  authorProviderEndpoint: text('author_provider_endpoint').notNull(),
+}).as(sql`SELECT
+    posts.*,
+    users.name as author_name,
+    users.web_id as author_web_id,
+    users.provider_endpoint as author_provider_endpoint
+  FROM posts
+  INNER JOIN users on posts.author_id = users.id
+  WHERE posts.is_public = true`)
