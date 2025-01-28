8f306e265331417979bf62a5a0f37d67ca420fd6
Clara <honjes@protonmail.com>
2025-01-28 11:03:49 +0100

    add better docs to apiClient class

--- frontend/src/controller/api.ts
+++ frontend/src/controller/api.ts
@@ -8,14 +8,26 @@ export interface ApiResponse<T> {
   status: number
 }
 
+/**
+ * ApiClient is a class that handles all requests to the api and handles non 200 responses
+ */
 export class ApiClient {
   baseUrl: string
   authStore = useAuthStore()
 
+  /**
+   * Constructor
+   * Sets the baseUrl to the import.meta.env.VITE_API_URL
+   */
   constructor() {
     this.baseUrl = import.meta.env.VITE_API_URL
   }
 
+  /**
+   * Sign up a new user
+   * @param {SignUpBody} body - body that is sent to the api
+   * @returns ApiResponse<SignInResponse | ApiErrors>
+   */
   async signup(body: SignUpBody): Promise<ApiResponse<SignInResponse | string | ApiErrors>> {
     try {
       const response = await ky.post(`${this.baseUrl}/signup`, { json: body })
@@ -30,7 +42,7 @@ export class ApiClient {
 
   /**
    * Sign in a user
-   * @param body - SignInBody
+   * @param {SignInBody} body - body that is sent to the api
    * @returns ApiResponse<SignInResponse | ApiErrors>
    */
   async signin(body: SignInBody): Promise<ApiResponse<SignInResponse | ApiErrors>> {
@@ -45,6 +57,11 @@ export class ApiClient {
     }
   }
 
+  /**
+   * Handles errors that are thrown by ky (all responses that are not 200 are handled here)
+   * @param {unknown} e - error that is thrown
+   * @returns ApiResponse<ApiErrors>
+   */
   async handleError(e: unknown): Promise<ApiResponse<ApiErrors>> {
     if (e instanceof HTTPError) {
       const errorMessage = await e.response.text()
