2e0ee7e710bcae2bfdd20eabee66d2935322c892
Clara <honjes@protonmail.com>
2025-01-29 12:16:54 +0100

    code: clean up User class

--- api/src/decorater/User.ts
+++ api/src/decorater/User.ts
@@ -1,17 +1,13 @@
 import type { SelectUsers } from "../types"
 
 export default class User {
-  userId: number
-  userName: string
-  token: string
-  endpoint: string
+  userId: number = 0
+  userName: string = ''
+  token: string = ''
+  provider: string = ''
+  endpoint: string = ''
 
   constructor(dbUser?: SelectUsers, token?: string) {
-    this.userId = 0
-    this.userName = ''
-    this.token = ''
-    this.endpoint = ''
-
     if (dbUser) {
       this.userId = dbUser.id
       this.userName = dbUser.name
@@ -33,20 +29,7 @@ export default class User {
     return `${this.endpoint}/${this.userName}`
   }
 
-  setUser(userId: number, userName: string, token: string, endpoint: string) {
-    this.userId = userId
-    this.userName = userName
-    this.token = token
-    this.endpoint = endpoint
   }
 
-  setUsername(userName: string) {
-    const userIdSplit = userName.split('/')
-    this.userName = userName
-    this.endpoint = userIdSplit[userIdSplit.length - 2]
-  }
-
-  setToken(token: string) {
-    this.token = token
   }
 }
