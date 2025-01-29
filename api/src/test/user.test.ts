d91ed22ce7017d359fef84a4c1d53f229e762c68
Clara <honjes@protonmail.com>
2025-01-29 13:30:35 +0100

    feat: add util function to handle webIds

---
+++ api/src/test/user.test.ts
@@ -0,0 +1,57 @@
+import { describe, expect, test } from 'bun:test'
+import { decodeWebId, encodeWebId } from '@/util/user'
+import User from '@/decorater/User'
+
+describe('User Util', () => {
+  describe('decodeWebId', () => {
+    test('decodeWebId', () => {
+      const webId = '@test@memory.'
+      const encoded = decodeWebId(webId)
+
+      expect(encoded.username).toBe('test')
+      expect(encoded.provider).toBe('memory.')
+      expect(encoded.endpoint).toBe('http://localhost:3000')
+    })
+
+    test('decodeWebId - with a not viably pod provider', () => {
+      const webId = '@test@test.'
+
+      expect(() => {
+        decodeWebId(webId)
+      }).toThrow('The provider is not a viable pod provider')
+    })
+  })
+  describe('encodeWebId', () => {
+    test('encodeWebId', () => {
+      const webId = '@test@memory.'
+      const encoded = encodeWebId('http://localhost:3000/test')
+
+      expect(encoded).toBe(webId)
+    })
+
+    test('encodeWebId - with User object', () => {
+      const webId = '@test@memory.'
+
+      const testUser = new User()
+      testUser.loadUser(JSON.stringify({ userId: 1, username: 'test', token: 'test', provider: 'memory.' }))
+      const encoded = encodeWebId(testUser)
+      expect(encoded).toBe(webId)
+    })
+
+    test('encodeWebId - with a not viably pod provider webId', () => {
+      expect(() => {
+        encodeWebId('notValidProvider/test')
+      }).toThrow('The provider is not a viable pod provider')
+    })
+
+    test('encodeWebId - with a not viably user object', () => {
+      expect(() => {
+        const invalidUser = new User()
+        invalidUser.loadUser(
+          JSON.stringify({ userId: 1, username: 'test', token: 'test', provider: 'notAPodProvider.' })
+        )
+        encodeWebId(invalidUser)
+      }).toThrow('The provider is not a viable pod provider')
+    })
+  })
+})
---
+++ api/src/util/user.ts
@@ -0,0 +1,49 @@
+import type User from '@/decorater/User'
+import { podProviderEndpoint, ViablePodProvider } from '@/types'
+
+/**
+ * Encodes a webId to a partial User object
+ * @param {string} webId - The webId to encode (e.g. @test@memory.)
+ * @returns
+ */
+export function decodeWebId(webId: string): {
+  username: string
+  provider: string
+  endpoint: string
+} {
+  const [username, provider] = webId.split('@').slice(1)
+
+  if (!Object.keys(podProviderEndpoint).includes(provider)) throw new Error('The provider is not a viable pod provider')
+
+  return {
+    username: username,
+    provider: provider,
+    endpoint: podProviderEndpoint[provider as ViablePodProvider]
+  }
+}
+
+/**
+ * Decodes a User object to a webId
+ * @param {User} string - The username of the user
+ * @returns {string} - The webId of the user
+ */
+export function encodeWebId(user: User): string
+export function encodeWebId(providerWebId: string): string
+export function encodeWebId(user_or_providerWebId: User | string): string {
+  if (typeof user_or_providerWebId === 'string') {
+    const username = user_or_providerWebId.split('/').pop()
+    const endpoint = user_or_providerWebId.split('/').slice(0, -1).join('/')
+    let name = ''
+    Object.entries(podProviderEndpoint).forEach(([providerName, providerEndpoint]) => {
+      if (providerEndpoint === endpoint) {
+        name = providerName
+      }
+    })
+    if (name === '') throw new Error('The provider is not a viable pod provider')
+    return `@${username}@${name}`
+  } else {
+    if (!Object.keys(podProviderEndpoint).includes(user_or_providerWebId.provider))
+      throw new Error('The provider is not a viable pod provider')
+    return `@${user_or_providerWebId.username}@${user_or_providerWebId.provider}`
+  }
+}
