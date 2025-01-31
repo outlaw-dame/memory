061d58a60f0df7fd789b24ef769286432467e2b7
Clara <honjes@protonmail.com>
2025-01-31 11:54:32 +0100

    code: remove unused import

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Vuesax from 'vuesax-alpha'
 import VueSaxIcon from '@/components/VueSaxIcon.vue'
-import { VsxIcon } from 'vue-iconsax'
 
 import App from './App.vue'
 import router from './router'
