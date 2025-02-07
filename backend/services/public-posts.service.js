14b3d69e8302b0339a9fe28a3055f4c6f103fbec
briidee3 <briidee3@gmail.com>
2025-02-07 09:50:25 -0500

    Revamped public-posts service, now primarily interacting with OpenSearch, added get action to handle getting public posts, added handling for activity creation/updating/deletion

--- backend/services/public-posts.service.js
+++ backend/services/public-posts.service.js
@@ -1,62 +1,163 @@
-// TODO:
-//  - [ ] set up a queue to handle incoming public posts when called here
-//  - [ ] set up a thing to constantly check if queue is non-empty
-//  - [ ] set up a thing to update RedPanda database w/ new public posts
+/* 
+This service is intended for use creating/updating/deleting posts tagged as public
+on a local elasticsearch instance.
+ */
+
+/*
+TODO:
+  - opensearch security plugin integration
+  - authentication for each step of the process (e.g. authenticate user before updating post in database)
+    as well as on create/update/delete of a post
+*/
 
 const path = require('path');
 //const { PodActivitiesWatcher } = require('@activitypods/app');
 const { PodResourcesHandlerMixin } = require('@activitypods/app');
 const { apods, notify, interop, oidc } = require('@semapps/ontologies');
+const { Client } = require('@opensearch-project/opensearch');
+//const fs = require('fs')
 const CONFIG = require('../config/config');
-const { Kafka } = require('kafkajs');
 
-const redpanda = new Kafka({
-  brokers: [`${CONFIG.REDPANDA_BROKERS}`.split(",")],
-});
-const producer = redpanda.producer();
+const opensearchAddress = "http://localhost:9200";
+const indexName = "public-posts"
 
 module.exports = {
-  name: 'activities',
+  name: 'public-posts',
   mixins: [PodResourcesHandlerMixin],
   settings: {
     type: 'as:Activity'
   },
+  // Initialize the opensearch client for connecting to the opensearch instance at the given address
+  started() {
+    this.client = new Client({
+      node: opensearchAddress,
+      /*
+      // for use with the security plugin
+      ssl: {
+        ca: fs.readFileSync(ca_certs_path),
+      },*/
+    });
+  },
   methods: {
+    // Automatically create post in opensearch on activity creation
     async onCreate(ctx, resource, actorUri) {
       try {
-        // Connect to Redpanda as a producer
-        await producer.connect();
-
-        // Upload activity to Redpanda if it's public
-        //if (resource.to?.findIndex("https://www.w3.org/ns/activitystreams#Public") !== -1) {
-        await producer.send({
-          topic: `${CONFIG.REDPANDA_PUBLIC_TOPIC}`,
-          data: [{ 
-            value: JSON.stringify(
-              {
-                activityUri: resource.id || resource['@id'],
-                actorUri: actorUri,
-                tag: resource.tag,
-                updated: Date.now(),
-                parentCtx: ctx
-              }
-            )
-          }]
+        return await this.client.index({
+          index: indexName,
+          id: encodeURIComponent(resource.id || resource['@id']),
+          body: {  
+            activityUri: resource.id || resource['@id'],
+            actorUri: actorUri,
+            content: resource.content,
+            tag: resource.tag,
+            parentCtx: ctx,
+            updateTime: Date.now()
+          },
+          refresh: true,
         });
-        //}
-
-        // Disconnect from Redpanda
-        await producer.disconnect();
       } 
       catch (error) {
-        console.error("Error creating activity in `public-posts.service`: ", error);
-      }      
+        console.error("Error creating activity in `public-posts.service`:\n\t", error);
+        return error;
+      }
     },
+    // Automatically update post in opensearch on activity update
     async onUpdate(ctx, resource, actorUri) {
-      // Handle post-update actions
+      try {
+        // Handle post-update actions
+        if ( ctx === this.client.get(encodeURIComponent(resource.id || resource['@id'])).body.parentCtx ) {   // check if same parent context
+          return await this.client.update({
+            index: indexName,
+            id: encodeURIComponent(resource.id || resource['@id']),
+            body: {
+              doc: {
+                content: resource.content,
+                tag: resource.tag,
+                updateTime: Date.now()
+              }
+            }
+          });
+        }
+        else {
+          return "Invalid context."
+        }
+      } 
+      catch (error) {
+        console.error("Error updating activity in `public-posts.service`:\n\t", error);
+        return error;
+      }
     },
+    // Automatically delete post in opensearch on activity deletion
     async onDelete(ctx, resource, actorUri) {
-      // Handle post-delete actions
+      try {
+        // Handle post-delete actions
+        if ( ctx === this.client.get(encodeURIComponent(resource.id || resource['@id'])).body.parentCtx ) {   // check if same parent context
+          return await this.client.delete({
+            index: indexName,
+            id: encodeURIComponent(resource.id || resource['@id']),
+          });
+        }
+        else {
+          return "Invalid context."
+        }
+      }
+      catch (error) {
+        console.error("Error deleting activity in `public-posts.service`:\n\t", error);
+        return error;
+      }
+    }
+  },
+  actions: {
+    // Fetch public timeline, optionally filtered by tag
+    get: {
+      params: {
+        initTime: { type: 'integer', optional: false },   // unix time of page load in ms since Jan 1 1970
+        nextPage: { type: 'integer', optional: false },   // used to get next page; first page should be equal to initTime
+        tag: { type: 'string', optional: true },
+        isFedTimeline: { type: 'bool', optional: true },
+      },
+      async handler(ctx) {
+        const { initTime, nextPage, tag, isFedTimeline } = ctx.params;
+
+        // Put together query based on tags and initial time of request
+        const query = {
+          size: 10,
+          query: {
+            // Only get posts older than the given time of the query
+            range: {
+              updateTime: {
+                lte: initTime,
+              }
+            },
+            // When given a tag, search for all which include at least one instance of the given tag
+            terms_set: {
+              tag: {
+                terms: [ tag ],
+                minimum_should_match_script: {
+                  source: "1"
+                }
+              }
+            }
+          },
+          search_after: [ nextPage ],
+          sort: [
+            {
+              updateTime: "desc",
+            }
+          ]
+        };
+
+        // Handle federated timeline (not implemented yet)
+        /*if (isFedTimeline) {
+
+        }*/
+        // Handle local timeline (default)
+        //else {
+        return await this.client.search({
+          index: indexName,
+          body: query
+        });
+      }
     }
   }
 };
