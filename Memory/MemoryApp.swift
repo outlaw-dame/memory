cc3f7f9290dd04ee201df5a2cc63d06a798db902
Damon Outlaw <damon@damonoutlaw.xyz>
2025-06-25 16:25:59 -0400

    Initial Commit

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Damon Outlaw on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoryTests/MemoryTests.swift
@@ -0,0 +1,16 @@
+//
+//  MemoryTests.swift
+//  MemoryTests
+//
+//  Created by Damon Outlaw on 6/25/25.
+//
+
+import Testing
+
+struct MemoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
